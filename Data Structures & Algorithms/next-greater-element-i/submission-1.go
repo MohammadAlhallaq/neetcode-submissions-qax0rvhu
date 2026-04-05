4541a738194dcbee5423ab939c792973c4a21256
Mohammad <Mohammad.alhallaq1@gmail.com>
2026-04-05 19:04:59 +0300

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.go
@@ -0,0 +1,27 @@
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	stack := []int{}
+	nextGreater := make(map[int]int)
+
+	// Build next greater map using nums2
+	for _, num := range nums2 {
+		for len(stack) > 0 && num > stack[len(stack)-1] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			nextGreater[top] = num
+		}
+		stack = append(stack, num)
+	}
+
+	// Remaining elements have no next greater
+	for _, num := range stack {
+		nextGreater[num] = -1
+	}
+
+	// Build result for nums1
+
+	result := make([]int, len(nums1))
+	for k, v := range nums1{
+		result[k] = nextGreater[v]
+	}
+	return result
+}
