c592abd2622697f8c8137fb933083cacd8cb88a0
Mohammad <Mohammad.alhallaq1@gmail.com>
2026-05-15 13:30:57 +0300

    Add: can-place-flowers - submission-0

---
+++ Data Structures & Algorithms/can-place-flowers/submission-0.go
@@ -0,0 +1,26 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	planted := 0
+
+	start := 0
+	end := len(flowerbed) - 1
+
+	for i := 0; i < len(flowerbed); i++ {
+
+		if flowerbed[i] == 1 {
+			continue
+		}
+
+		leftEmpty := (i == start || flowerbed[i-1] == 0)
+		rightEmpty := (i == end || flowerbed[i+1] == 0)
+
+		if leftEmpty && rightEmpty {
+			flowerbed[i] = 1
+			planted++
+		}
+
+		if planted >= n {
+			return true
+		}
+	}
+	return planted >= n
+}
