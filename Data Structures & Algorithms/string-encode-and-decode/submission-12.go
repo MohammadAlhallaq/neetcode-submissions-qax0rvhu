c579adb883056607a44e09f791854772c20ae3b5
Mohammad <Mohammad.alhallaq1@gmail.com>
2026-06-22 15:35:34 +0300

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.go
@@ -0,0 +1,30 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	encoded := ""
+	for i := 0; i < len(strs); i++ {
+		curr := strs[i]
+		length := len(curr)
+		encoded += strconv.Itoa(length) + "#" + curr
+	}
+	return encoded
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	index := 0
+	res := []string{}
+
+	for index < len(encoded) {
+		j := index
+		for j < len(encoded) && unicode.IsDigit(rune(encoded[j])) {
+			j++
+		}
+		numserStr := encoded[index:j]
+		number, _ := strconv.Atoi(numserStr)
+		start := j + 1
+		end := start + number
+		res = append(res, encoded[start:end])
+		index = end
+	}
+	return res
+}
